7e34009d1a4346ca3f2d5d9e878be80e31401add
mboussaa <mohamed.boussaa@inria.fr>
2016-06-06 10:10:22 +0000

    update

--- core/test/TS2.hx
+++ core/test/TS2.hx
@@ -11,7 +11,8 @@ trace('The value of loop_wrapper is $w');
 
 var x : Int = Std.parseInt(w);
  //   runner.addCase(new thx.TestAnonymousMap(x));
-//    runner.addCase(new thx.TestArrays(x));
+runner.addCase(new thx.fp.TestState(x));
+//      runner.addCase(new thx.fp.TestWriter(x));
  //   runner.addCase(new thx.TestAssert(x));
  //   runner.addCase(new thx.TestBigInt(x));
  //   runner.addCase(new thx.TestBitMatrix(x));
